7b2bc8b02a9fc5c14aa85c0b150431bdb9209bf6
Josh <armyboy@gmail.com>
2022-08-27 17:37:37 -0400

    Lez Price Change proposals
    comments in file

--- Misc.lua
+++ Misc.lua
@@ -1,7 +1,7 @@
-Shop.Items["Base.LargeIcePack"] = {
+Shop.Items["Base.LargeIcePack"] = {  --not sure the usage for valuation?
 	tab = Tab.Misc, price = 30,
 }
-Shop.Items["Base.SmallIcePack"] = {
+Shop.Items["Base.SmallIcePack"] = {  --not sure the usage for valuation?
 	tab = Tab.Misc, price = 15,
 }
 Shop.Items["Base.RedCooler"] = {
@@ -10,37 +10,37 @@ Shop.Items["Base.RedCooler"] = {
 Shop.Items["Base.OrganCooler"] = {
 	tab = Tab.Misc, price = 100,
 }
-Shop.Items["Base.Bag_InsulatedPack"] = {
+Shop.Items["Base.Bag_InsulatedPack"] = {  --a form of backpack iirc, don't remember size.  Might need price increase?
 	tab = Tab.Misc, price = 200,
 }
 Shop.Items["VPT.BusTicket"] = {
 	tab = Tab.Misc, price = 5,
 }
 Shop.Items["Base.Cigarettes"] = {
-	tab = Tab.Misc, price = 5,
+	tab = Tab.Misc, price = 10,  --doubled cost per cig.
 }
 Shop.Items["Base.Soap2"] = {
-	tab = Tab.Misc, price = 5,
+	tab = Tab.Misc, price = 50, --did anyone really use soap?  increased to encourage looting.
 }
 Shop.Items["Base.CandleLit"] = {
-	tab = Tab.Misc, price = 10,
+	tab = Tab.Misc, price = 25, --crappy consumable flashlight?
 }
 Shop.Items["Base.Matches"] = {
-	tab = Tab.Misc, price = 10,
+	tab = Tab.Misc, price = 15,  --plentiful to be looted from zombies, but cheap enough that newnie smokers can get them it no one is trading
 }
 Shop.Items["Base.Lighter"] = {
-	tab = Tab.Misc, price = 15,
+	tab = Tab.Misc, price = 100,  --refillable, plentiful to be looted from zombies.  Price increased to strongly encourage looting/trade
 }
 Shop.Items["Base.CampfireKit"] = {
-	tab = Tab.Misc, price = 50,
+	tab = Tab.Misc, price = 75,  --east to craft, encourage that.
 }
 Shop.Items["Base.CampingTentKit"] = {
-	tab = Tab.Misc, price = 50,
+	tab = Tab.Misc, price = 75,  --fairly easy to craft, not used except for "RP", increased to match Campfire
 }
 Shop.Items["Base.PetrolCan"] = {
-	tab = Tab.Misc, price = 200,
+	tab = Tab.Misc, price = 250, --increased to 250, just to try to balance with other increases.
 }
-Shop.Items["Base.Sparklers"] = {
+Shop.Items["Base.Sparklers"] = {  --I think this should be moved to Crafting: https://pzwiki.net/wiki/Box_of_Sparklers
 	tab = Tab.Misc, price = 50,
 }
 Shop.Items["Base.Lunchbox"] = {
@@ -65,7 +65,7 @@ Shop.Items["Base.Keytar"] = {
 	tab = Tab.Misc, price = 250,
 }
 Shop.Items["Base.Remote"] = {
-	tab = Tab.Misc, price = 25,
+	tab = Tab.Misc, price = 50, --increased to 50 to encourage looting, not used much is it?
 }
 Shop.Items["Tsarcraft.TCBoombox"] = {
 	tab = Tab.Misc, price = 50,
@@ -73,38 +73,38 @@ Shop.Items["Tsarcraft.TCBoombox"] = {
 Shop.Items["Base.Generator"] = {
 	tab = Tab.Misc, price = 350,
 }
-Shop.Items["Base.Needle"] = {
-	tab = Tab.Misc, price = 10,
+Shop.Items["Base.Needle"] = {  --maybe move to crafting?
+	tab = Tab.Misc, price = 75,  --increased to 75.  You only need 1, to encourage looting for them, but they are a big gatekeep tool for tailoring.
 }
-Shop.Items["Base.Timer"] = {
-	tab = Tab.Misc, price = 25,
+Shop.Items["Base.Timer"] = {  --useful for noisemaker crafting?
+	tab = Tab.Misc, price = 75,  --increased to 75 to encourage looting of alarm clocks.
 }
 Shop.Items["Base.Bleach"] = {
-	tab = Tab.Misc, price = 10,
+	tab = Tab.Misc, price = 25,  --increased a little.  Probably should be higher, but it makes a great easy-access to a suicide item.
 }
 Shop.Items["Base.Mop"] = {
-	tab = Tab.Misc, price = 15,
+	tab = Tab.Misc, price = 25,
 }
-Shop.Items["Base.Torch"] = {
-	tab = Tab.Misc, price = 30,
+Shop.Items["Base.Torch"] = { --big yellow flashlight
+	tab = Tab.Misc, price = 75,  --from 30 to 75 to encourage looting/trade
 }
-Shop.Items["Base.HandTorch"] = {
-	tab = Tab.Misc, price = 30,
+Shop.Items["Base.HandTorch"] = {  --flashlight
+	tab = Tab.Misc, price = 75, --from 30 to 75 to encourage looting/trade
 }
-Shop.Items["Base.FishingRod"] = {
-	tab = Tab.Misc, price = 75,
+Shop.Items["Base.FishingRod"] = {  --this is the "commercial, pre-fall" version, with fishing line.  Maybe swap for crafted or lineless version?  Can't see a gameplay difference.
+	tab = Tab.Misc, price = 150,  --doubled price to encourage crafting rods, or trading/looting for the commercial one here
 }
 Shop.Items["Base.FishingTackle"] = {
 	tab = Tab.Misc, price = 45,
 }
 Shop.Items["Base.FishingLine"] = {
-	tab = Tab.Misc, price = 40,
+	tab = Tab.Misc, price = 75, --increased to 75 to encourage looting/trade
 }
 Shop.Items["Base.FishingNet"] = {
-	tab = Tab.Misc, price = 60,
+	tab = Tab.Misc, price = 75, --increase to encourage crafting
 }
 Shop.Items["Base.SleepingbagRolled"] = {
-	tab = Tab.Misc, price = 30,
+	tab = Tab.Misc, price = 100,  --increased to encourage looting/trading.  light mobile bed.
 }
 Shop.Items["Base.BoxOfJars"] = {
 	tab = Tab.Misc, price = 100,
@@ -113,9 +113,9 @@ Shop.Items["Base.Battery"] = {
 	tab = Tab.Misc, price = 100,
 }
 Shop.Items["Base.Newspaper"] = {
-	tab = Tab.Misc, price = 20,
+	tab = Tab.Misc, price = 50,  --increased to encourage looting
 }
-Shop.Items["Base.FishingMag1"] = {
+Shop.Items["Base.FishingMag1"] = { --is there a reason these magazines are available?
     tab= Tab.Misc, price= 250,
 }
 Shop.Items["Base.FishingMag2"] = {
@@ -127,14 +127,14 @@ Shop.Items["Base.ElectronicsMag4"] = {
 Shop.Items["Base.HerbalistMag"] = {
     tab= Tab.Misc, price= 300,
 }
-Shop.Items["Base.LightBulb"] = {
-    tab= Tab.Misc, price= 10,
+Shop.Items["Base.LightBulb"] = {  --obtainable from dismanblting walkietalkies on zombies, usable to replace headlights (thus the additional cost)
+    tab= Tab.Misc, price= 75,
 }
 Shop.Items["Base.LightBulbRed"] = {
     tab= Tab.Misc, price= 10,
 }
-Shop.Items["Base.LightBulbGreen"] = {
-    tab= Tab.Misc, price= 10,
+Shop.Items["Base.LightBulbGreen"] = {  --onbtainable from dismantling walkietalkies on zombies
+    tab= Tab.Misc, price= 25,
 }
 Shop.Items["Base.LightBulbBlue"] = {
     tab= Tab.Misc, price= 10,
@@ -158,12 +158,12 @@ Shop.Items["Base.LightBulbPink"] = {
     tab= Tab.Misc, price= 10,
 }
 Shop.Items["Base.Flashlight_Military"] = {
-    tab= Tab.Misc, price= 100,
+    tab= Tab.Misc, price= 150,  --priced to match the increase of vanilla flashlights
 }
-Shop.Items["Defecation.AntiDiarrhealPillBox"] = {
-    tab= Tab.Misc, price= 30,
+Shop.Items["Defecation.AntiDiarrhealPillBox"] = {  --move to first aid?
+    tab= Tab.Misc, price= 100,  --price to match pills in first aid tab/file.
 }
-Shop.Items["Base.Padlock"] = {
+Shop.Items["Base.Padlock"] = {  --is it useful?
     tab= Tab.Misc, price= 25,
 }
 Shop.Items["Base.MotionSensor"] = {

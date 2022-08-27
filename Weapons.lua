9be8bb1073f8f2a649a76ef37af3407f656c9899
Josh <armyboy@gmail.com>
2022-08-27 17:49:13 -0400

    Organizing Tab into commented sub-categories (Ammo, Mags, etc)

--- Weapons.lua
+++ Weapons.lua
@@ -1,13 +1,8 @@
+--Ammo Boxes
 Shop.Items["Base.ShotgunShellsBox"] = {
 	tab = Tab.Weapons, price = 50,
 }
 Shop.Items["Base.Bullets9mmBox"] = {
-	tab = Tab.Weapons, price = 50,
-}
-Shop.Items["Base.9mmClip"] = {
-	tab = Tab.Weapons, price = 50,
-}
-Shop.Items["Base.556Clip"] = {
 	tab = Tab.Weapons, price = 50,
 }
 Shop.Items["Base.556Box"] = {
@@ -16,15 +11,20 @@ Shop.Items["Base.556Box"] = {
 Shop.Items["Base.223Box"] = {
 	tab = Tab.Weapons, price = 50,
 }
-Shop.Items["Base.223Clip"] = {
-	tab = Tab.Weapons, price = 50,
-}
 Shop.Items["Base.308Box"] = {
 	tab = Tab.Weapons, price = 50,
 }
-Shop.Items["Base.308Clip"] = {
+Shop.Items["Base.Bullets45Box"] = {
 	tab = Tab.Weapons, price = 50,
 }
+Shop.Items["Base.Bullets44Box"] = {
+	tab = Tab.Weapons, price = 50,
+}
+Shop.Items["Base.Bullets38Box"] = {
+	tab = Tab.Weapons, price = 50,
+}
+
+--Gun Accessories
 Shop.Items["Base.AmmoStraps"] = {
 	tab = Tab.Weapons, price = 100,
 }
@@ -34,27 +34,37 @@ Shop.Items["Base.IronSight"] = {
 Shop.Items["Base.x4Scope"] = {
 	tab = Tab.Weapons, price = 300,
 }
-Shop.Items["Base.SmokeBomb"] = {
-	tab = Tab.Weapons, price = 100,
+Shop.Items["Base.RedDot"] = {
+    tab= Tab.Weapons, price= 150,
 }
-Shop.Items["Base.Gunpowder"] = {
-	tab = Tab.Weapons, price = 50,
+Shop.Items["Base.Laser"] = {
+    tab= Tab.Weapons, price= 200,
 }
-Shop.Items["Base.Bullets45Box"] = {
-	tab = Tab.Weapons, price = 50,
-}
+
+--Gun Magazines
 Shop.Items["Base.45Clip"] = {
 	tab = Tab.Weapons, price = 50,
 }
 Shop.Items["Base.44Clip"] = {
 	tab = Tab.Weapons, price = 50,
 }
-Shop.Items["Base.Bullets44Box"] = {
+Shop.Items["Base.9mmClip30"] = {
+    tab= Tab.Weapons, price= 50,
+}
+Shop.Items["Base.9mmClip"] = {
 	tab = Tab.Weapons, price = 50,
 }
-Shop.Items["Base.Bullets38Box"] = {
+Shop.Items["Base.556Clip"] = {
 	tab = Tab.Weapons, price = 50,
 }
+Shop.Items["Base.223Clip"] = {
+	tab = Tab.Weapons, price = 50,
+}
+Shop.Items["Base.308Clip"] = {
+	tab = Tab.Weapons, price = 50,
+}
+
+--Melee Weapons
 Shop.Items["Base.HuntingKnife"] = {
     tab= Tab.Weapons, price= 75,
 }
@@ -64,12 +74,11 @@ Shop.Items["Base.Crowbar"] = {
 Shop.Items["Base.BaseballBatNails"] = {
     tab= Tab.Weapons, price= 250,
 }
-Shop.Items["Base.9mmClip30"] = {
-    tab= Tab.Weapons, price= 50,
+
+--Misc Items
+Shop.Items["Base.SmokeBomb"] = {
+	tab = Tab.Weapons, price = 100,
 }
-Shop.Items["Base.RedDot"] = {
-    tab= Tab.Weapons, price= 150,
-}
-Shop.Items["Base.Laser"] = {
-    tab= Tab.Weapons, price= 200,
+Shop.Items["Base.Gunpowder"] = {
+	tab = Tab.Weapons, price = 50,
 }

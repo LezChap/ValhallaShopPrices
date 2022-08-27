47c9cce07a33a3a27698572421d2241e05f06cbc
Josh <armyboy@gmail.com>
2022-08-27 16:50:48 -0400

    LezChap's suggested cost changes

--- FirstAid.lua
+++ FirstAid.lua
@@ -1,27 +1,27 @@
 Shop.Items["Base.Bandaid"] = {
-	tab = Tab.FirstAid, price = 15,
+	tab = Tab.FirstAid, price = 50, --change from 15 to 50.  They're reusable, afterall.
 }
 Shop.Items["Base.Splint"] = {
-	tab = Tab.FirstAid, price = 10,
+	tab = Tab.FirstAid, price = 50,  --change from 15 to 50 to encourage crafting from ripped sheets from zombies and tree branches
 }
-Shop.Items["Base.Pills"] = {
-	tab = Tab.FirstAid, price = 50,
+Shop.Items["Base.Pills"] = { --Painkillers
+	tab = Tab.FirstAid, price = 100,  --increase 50 to 100.  They're pretty plentiful from looting zombies and clinics.
 }
 Shop.Items["Base.AlcoholBandage"] = {
-	tab = Tab.FirstAid, price = 50,
+	tab = Tab.FirstAid, price = 150,  --chanage from 50 to 150.  They're just a boiled bandage, reusable.
 }
 Shop.Items["Base.Disinfectant"] = {
-	tab = Tab.FirstAid, price = 50,
+	tab = Tab.FirstAid, price = 100,  --incrased from 50 to 100.  Main use afaik is disinfecting bandages.
 }
 Shop.Items["Base.Antibiotics"] = {
-	tab = Tab.FirstAid, price = 50,
+	tab = Tab.FirstAid, price = 100,  --other than fighting colds, does anyone actually use these?  They're fairly rare loot.
 }
-Shop.Items["Base.PillsAntiDep"] = {
-	tab = Tab.FirstAid, price = 75,
+Shop.Items["Base.PillsAntiDep"] = { --antidepressents
+	tab = Tab.FirstAid, price = 150,  --increase from 75 to 100.
 }
 Shop.Items["Base.PillsBeta"] = {
-	tab = Tab.FirstAid, price = 50,
+	tab = Tab.FirstAid, price = 100,  --increased from 50 to 100.
 }
 Shop.Items["Base.PillsVitamins"] = {
-	tab = Tab.FirstAid, price = 50,
+	tab = Tab.FirstAid, price = 75,  --increased from 50 to 75.  I don't think they're very useful, and very plentiful loot.
 }
